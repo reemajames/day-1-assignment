39c4123569ef1ec3cd191dee57d5be7a63c0517d
reemajames <160592881+reemajames@users.noreply.github.com>
2024-02-21 11:37:07 +0530

    Node.js sample code and advantages

--- Day-1-assignment/index.js
+++ Day-1-assignment/index.js
@@ -1,18 +1,36 @@
 const fs = require('fs');
 
 //create and write to a file
+function createFile(){
 fs.writeFileSync('./testfile', 'this is a new file');
+}
 
 //read the file
+function readFile(){
 const file_content = fs.readFileSync('./testfile').toString();
 console.log(file_content);
+}
 
 //update the file
+function updateFile(){
 fs.appendFileSync('./testfile', " this is updated file content");
-
 const updated_file_content = fs.readFileSync('./testfile').toString();
 console.log(updated_file_content);
+}
 
 //delete the file
+function deleteFile(){
 fs.unlinkSync('./deletefile');
+}
 
+createFile();
+readFile();
+updateFile();
+deleteFile();
+
+module.exports = {
+    createFile,
+    readFile,
+    updateFile,
+    deleteFile
+}
